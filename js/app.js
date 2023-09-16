a52b15f3bdf5e3325b4bcf17d757eb2c28a03cfe
andrewwinke123 <andrewwinke@gmail.com>
2023-09-16 12:28:00 -0400

    handling the player clicking a button

--- js/app.js
+++ js/app.js
@@ -7,12 +7,13 @@ let playersChoice, computersChoice, msg
 
 
 /*------------------------ Cached Element References ------------------------*/
-
+const resultDisplayEl = document.getElementById('result-display')
 
 
 /*----------------------------- Event Listeners -----------------------------*/
-
+document.getElementById('rock').addEventListener('click', play)
+document.getElementById('paper').addEventListener('click', play)
+document.getElementById('scissors').addEventListener('click', play)
 
 
 /*-------------------------------- Functions --------------------------------*/
-
