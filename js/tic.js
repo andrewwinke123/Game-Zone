fc8a2c32677e4174a2b3a13294f449ac962b9bc2
andrewwinke123 <andrewwinke@gmail.com>
2023-09-21 22:47:48 -0400

    fixing position of reult-message because it went astray at some point

--- js/tic.js
+++ js/tic.js
@@ -3,7 +3,10 @@
 
 
 /*-------------------------------- Variables --------------------------------*/
-
+let board
+let turn
+let winner
+let tie
 
 /*------------------------ Cached Element References ------------------------*/
 const ticGameContainerEl = document.getElementById('tic-game-container')
@@ -13,3 +16,23 @@ const ticGameContainerEl = document.getElementById('tic-game-container')
 
 
 /*-------------------------------- Functions --------------------------------*/
+
+
+
+
+
+
+// 1) Define the required variables used to track the state of the game
+
+// 2) Store cached element references
+
+// 3) Upon loading, the game state should be initialized, and a function should be 
+//    called to render this game state
+
+// 4) The state of the game should be rendered to the user
+
+// 5) Define the required constants
+
+// 6) Handle a player clicking a square with a `handleClick` function
+
+// 7) Create Reset functionality
